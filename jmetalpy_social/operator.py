7be0b6e4b8826cc821a0ca3539c29a9847c5c7a3
Mateusz Wojtulewicz <mateusz.wojtulewicz@gmail.com>
2022-11-17 00:36:31 +0100

    new: repel operators, results after 4 repeats

--- jmetalpy_social/operator.py
+++ jmetalpy_social/operator.py
@@ -120,14 +120,10 @@ class FollowBestSharedGenesMutation(Mutation):
             )
 
             for g in chosen_genes:
-                mean_best = np.mean(
-                    [
-                        self.tracked_algorithm.solutions[j].variables[g]
-                        for j in range(self.tracked_best_count)
-                    ]
+                diff = (
+                    self.tracked_algorithm.solutions[teacher_idx].variables[g]
+                    - solution.variables[g]
                 )
-
-                diff = mean_best - solution.variables[g]
 
                 solution.variables[g] += self.follow_rate * diff
 
@@ -137,10 +133,18 @@ class FollowBestSharedGenesMutation(Mutation):
         return "FollowBestSharedGenesMutation"
 
 
-class FollowBestMutationSingleGene(Mutation):
-    def __init__(self, probability: float, tracked_best_count: int):
+class FollowBestDistinctGenesMutation(Mutation):
+    def __init__(
+        self,
+        probability: float,
+        tracked_best_count: int,
+        copy_genes_count: int,
+        follow_rate: float,
+    ):
         Mutation.__init__(self, probability)
         self.tracked_best_count = tracked_best_count
+        self.copy_genes_count = copy_genes_count
+        self.follow_rate = follow_rate
 
     def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
         self.tracked_algorithm = algorithm
@@ -149,125 +153,245 @@ class FollowBestMutationSingleGene(Mutation):
         rand = random.random()
 
         if rand < self.probability:
-            teacher_idx = random.randint(0, self.tracked_best_count - 1)
-            gene_swap_idx = random.randint(0, len(solution.variables) - 1)
 
-            solution.variables[gene_swap_idx] = self.tracked_algorithm.solutions[
-                teacher_idx
-            ].variables[gene_swap_idx]
+            teacher_idx = random.randint(0, self.tracked_best_count - 1)
+
+            genes_mat = np.array(
+                [
+                    sol.variables
+                    for sol in self.tracked_algorithm.solutions[
+                        : self.tracked_best_count
+                    ]
+                ]
+            )
+            genes_mat = np.transpose(genes_mat)
+
+            sharing_rating = np.std(genes_mat, axis=-1)
+            sharing_rating = np.exp(sharing_rating) / np.sum(np.exp(sharing_rating))
+
+            chosen_genes = np.random.choice(
+                np.arange(0, sharing_rating.shape[0]),
+                p=sharing_rating,
+                size=(self.copy_genes_count,),
+            )
+
+            for g in chosen_genes:
+                diff = (
+                    self.tracked_algorithm.solutions[teacher_idx].variables[g]
+                    - solution.variables[g]
+                )
+
+                solution.variables[g] += self.follow_rate * diff
 
         return solution
 
     def get_name(self):
-        return "FollowBestMutationSingleGene"
+        return "FollowBestDistinctGenesMutation"
 
 
-class AvoidWorstMutation(Mutation):
-    def __init__(self, probability: float, tracked_worst_count: int, avoid_rate: float):
+class RepelWorstMutation(Mutation):
+    def __init__(self, probability: float, tracked_worst_count: int, repel_rate: float):
         super().__init__(probability)
         self.tracked_worst_count = tracked_worst_count
-        self.avoid_rate = avoid_rate
+        self.repel_rate = repel_rate
 
     def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
         self.tracked_algorithm = algorithm
 
     def execute(self, solution):
         n = len(self.tracked_algorithm.solutions)
-        to_avoid_idx = n - random.randint(1, self.tracked_worst_count)
-        for i in range(len(solution.variables)):
-            diff = (
-                self.tracked_algorithm.solutions[to_avoid_idx].variables[i]
-                - solution.variables[i]
-            )
-            solution.variables[i] += self.avoid_rate * diff
-        return solution
-
-    def get_name(self) -> str:
-        return "AvoidWorstMutation"
-
-
-class AvoidWorstMutationSparse(Mutation):
-    def __init__(self, probability: float, tracked_worst_count: int, avoid_rate: float):
-        super().__init__(probability)
-        self.tracked_worst_count = tracked_worst_count
-        self.avoid_rate = avoid_rate
-
-    def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
-        self.tracked_algorithm = algorithm
-
-    def execute(self, solution):
-        n = len(self.tracked_algorithm.solutions)
-        to_avoid_idx = n - random.randint(1, self.tracked_worst_count)
-        for i in range(len(solution.variables)):
-            rand = random.random()
-            if rand < self.probability:
+        to_repel_idx = n - random.randint(1, self.tracked_worst_count)
+        if random.random() < self.probability:
+            for i in range(len(solution.variables)):
                 diff = (
-                    self.tracked_algorithm.solutions[to_avoid_idx].variables[i]
-                    - solution.variables[i]
+                    solution.variables[i]
+                    - self.tracked_algorithm.solutions[to_repel_idx].variables[i]
                 )
-                solution.variables[i] += self.avoid_rate * diff
+                solution.variables[i] += -self.repel_rate * (
+                    diff / self.tracked_algorithm.problem.lower_bound[i]
+                    - diff / abs(diff + 1e-6)
+                )
+                solution.variables[i] = min(
+                    max(
+                        solution.variables[i],
+                        self.tracked_algorithm.problem.lower_bound[i],
+                    ),
+                    self.tracked_algorithm.problem.upper_bound[i],
+                )
         return solution
 
     def get_name(self) -> str:
-        return "AvoidWorstMutationSparse"
+        return "RepelWorstMutation"
 
 
-class AvoidWorstMutationNorm(Mutation):
-    def __init__(self, probability: float, tracked_worst_count: int, norm_fn: callable):
+class RepelWorstMutationSparse(Mutation):
+    def __init__(self, probability: float, tracked_worst_count: int, repel_rate: float):
+        super().__init__(probability)
+        self.tracked_worst_count = tracked_worst_count
+        self.repel_rate = repel_rate
+
+    def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
+        self.tracked_algorithm = algorithm
+
+    def execute(self, solution):
+        n = len(self.tracked_algorithm.solutions)
+        to_repel_idx = n - random.randint(1, self.tracked_worst_count)
+        for i in range(len(solution.variables)):
+            if random.random() < self.probability:
+                diff = (
+                    solution.variables[i]
+                    - self.tracked_algorithm.solutions[to_repel_idx].variables[i]
+                )
+                solution.variables[i] += -self.repel_rate * (
+                    diff / self.tracked_algorithm.problem.lower_bound[i]
+                    - diff / abs(diff + 1e-6)
+                )
+                solution.variables[i] = min(
+                    max(
+                        solution.variables[i],
+                        self.tracked_algorithm.problem.lower_bound[i],
+                    ),
+                    self.tracked_algorithm.problem.upper_bound[i],
+                )
+        return solution
+
+    def get_name(self) -> str:
+        return "RepelWorstMutationSparse"
+
+
+class RepelWorstMutationNorm(Mutation):
+    def __init__(
+        self,
+        probability: float,
+        tracked_worst_count: int,
+        norm_fn: callable,
+        repel_rate: float,
+    ):
         super().__init__(probability)
         self.tracked_worst_count = tracked_worst_count
         self.norm_fn = norm_fn
+        self.repel_rate = repel_rate
 
     def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
         self.tracked_algorithm = algorithm
 
     def execute(self, solution):
         n = len(self.tracked_algorithm.solutions)
-        to_avoid_idx = n - random.randint(1, self.tracked_worst_count)
-        for i in range(len(solution.variables)):
-            diff = (
-                self.tracked_algorithm.solutions[to_avoid_idx].variables[i]
-                - solution.variables[i]
-            )
-            solution.variables[i] += self.norm_fn(diff)
+        to_repel_idx = n - random.randint(1, self.tracked_worst_count)
+        if random.random() < self.probability:
+            for i in range(len(solution.variables)):
+                diff = (
+                    solution.variables[i]
+                    - self.tracked_algorithm.solutions[to_repel_idx].variables[i]
+                )
+                solution.variables[i] += self.repel_rate * float(self.norm_fn(diff))
+                solution.variables[i] = min(
+                    max(
+                        solution.variables[i],
+                        self.tracked_algorithm.problem.lower_bound[i],
+                    ),
+                    self.tracked_algorithm.problem.upper_bound[i],
+                )
         return solution
 
     def get_name(self) -> str:
-        return "AvoidWorstMutationNorm"
+        return "RepelWorstMutationNorm"
 
 
-class AvoidWorstMutationMean(Mutation):
-    def __init__(self, probability: float, tracked_worst_count: int):
+class RepelWorstMutationMean(Mutation):
+    def __init__(
+        self,
+        probability: float,
+        tracked_worst_count: int,
+        repel_rate: float,
+    ):
         super().__init__(probability)
         self.tracked_worst_count = tracked_worst_count
+        self.repel_rate = repel_rate
 
     def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
         self.tracked_algorithm = algorithm
 
     def execute(self, solution):
         n = len(self.tracked_algorithm.solutions)
-        for i in range(len(solution.variables)):
-            mean_var = sum(
-                self.tracked_algorithm.solutions[n - j].variables[i]
-                for j in range(1, self.tracked_worst_count)
-            )
-            diff = mean_var - solution.variables[i]
-            solution.variables[i] += diff
+        if random.random() < self.probability:
+            for i in range(len(solution.variables)):
+                val = solution.variables[i]
+                fn = lambda x: -1 * (
+                    x / self.tracked_algorithm.problem.lower_bound[i]
+                    - x / abs(x + 1e-6)
+                )
+                diff = (
+                    sum(
+                        fn(val - self.tracked_algorithm.solutions[n - j].variables[i])
+                        for j in range(1, self.tracked_worst_count)
+                    )
+                    / self.tracked_worst_count
+                )
+                solution.variables[i] += self.repel_rate * diff
+                solution.variables[i] = min(
+                    max(
+                        solution.variables[i],
+                        self.tracked_algorithm.problem.lower_bound[i],
+                    ),
+                    self.tracked_algorithm.problem.upper_bound[i],
+                )
         return solution
 
     def get_name(self) -> str:
-        return "AvoidWorstMutationMean"
+        return "RepelWorstMutationMean"
 
 
-class AvoidWorstMutationSingleGene(Mutation):
-    pass
+class RepelWorstMutationGravity(RepelWorstMutationNorm):
+    def __init__(self, probability: float, tracked_worst_count: int, repel_rate: float):
+        gravity = lambda x: x / abs(x + 1e-6) * (abs(x) + 1) ** -2
+        super().__init__(
+            probability=probability,
+            tracked_worst_count=tracked_worst_count,
+            norm_fn=gravity,
+            repel_rate=repel_rate,
+        )
+
+    def get_name(self) -> str:
+        return "RepelWorstMutationGravity"
 
 
-"""
-TODO:
-- other mutations
-    - single gene
-    - some interesting ones :)
-- run simulations
-- plot results
-"""
+class RepelWorstMutationGravityMutlistep(Mutation):
+    def __init__(
+        self,
+        probability: float,
+        tracked_worst_count: int,
+        repel_rate: float,
+    ):
+        super().__init__(probability)
+        self.tracked_worst_count = tracked_worst_count
+        self.repel_rate = repel_rate
+
+    def set_tracked_algorithm(self, algorithm: GeneticAlgorithm):
+        self.tracked_algorithm = algorithm
+
+    def execute(self, solution):
+        n = len(self.tracked_algorithm.solutions)
+        if random.random() < self.probability:
+            for i in range(len(solution.variables)):
+                gravity = lambda x: x / abs(x + 1e-6) * (abs(x) + 1) ** -2
+                mean_val = (
+                    sum(
+                        self.tracked_algorithm.solutions[n - j].variables[i]
+                        for j in range(1, self.tracked_worst_count)
+                    )
+                    # / self.tracked_worst_count
+                )
+                diff = solution.variables[i] - mean_val
+                solution.variables[i] += self.repel_rate * gravity(diff)
+                solution.variables[i] = min(
+                    max(
+                        solution.variables[i],
+                        self.tracked_algorithm.problem.lower_bound[i],
+                    ),
+                    self.tracked_algorithm.problem.upper_bound[i],
+                )
+        return solution
+
+    def get_name(self) -> str:
+        return "RepelWorstMutationMean"
